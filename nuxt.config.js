4973375468193de4ae26c50da4c46ba89c102ce9
akaregi <akg.tachibana@gmail.com>
2020-08-02 02:07:37 +0900

    backend: Compressed config file
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,9 @@
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
   mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
+
   target: 'static',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+
   head: {
     title: process.env.npm_package_name || '',
     meta: [
@@ -25,50 +15,16 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/css/main.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+
+  css: ['~/css/main.css'],
+
   components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    // '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content'
-  ],
-  /*
-  ** Content module configuration
-  ** See https://content.nuxtjs.org/configuration
-  */
+
+  modules: ['@nuxt/content'],
+
   content: {
     markdown: {
       remarkPlugins: ['remark-breaks']
     }
-  },
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
   }
 }
