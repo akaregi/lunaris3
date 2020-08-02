0a3eec40bd02087cd3536c9f084771e1ecffca7c
akaregi <akg.tachibana@gmail.com>
2020-08-03 04:00:29 +0900

    frontend: Add footer
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
   <main>
     <nuxt />
+
+    <hr>
+
+    <footer>
+      <p>(C) 2020 AKANE AKAGI.</p>
+      <p>この作品は<a href="https://creativecommons.org/licenses/by/4.0/">クリエイティブ・コモンズ 表示 4.0 国際 ライセンス</a>で提供されます。</p>
+    </footer>
   </main>
 </template>
+
+<style scoped>
+p {
+  margin: 0;
+}
+
+footer {
+  margin: 4rem 0;
+}
+</style>
