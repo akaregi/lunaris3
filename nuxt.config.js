0ebbd96d40bb07297844863acba2653fcf4f4254
akaregi <akg.tachibana@gmail.com>
2020-08-03 03:16:52 +0900

    backend: Add route generation
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,15 @@ export default {
 
   modules: ['@nuxt/content'],
 
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
+
   content: {
     markdown: {
       remarkPlugins: ['remark-breaks']
