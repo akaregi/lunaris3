b5fb7001a1383913b733d1a5e6962d5a765f7e66
akaregi <akg.tachibana@gmail.com>
2020-10-21 21:29:01 +0900

    other: zipped css size
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
+  purge: [
+    './src/**/*.vue'
+  ],
   plugins: [
     require('@tailwindcss/typography')
   ],
